85b7867765ae27fba4aaa6fda5244426381f9df0
prenas <prenas@gmail.com>
2021-11-25 20:37:28 -0300

    Home ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuiensoyComponent } from './pages/quiensoy/quiensoy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '' , component :  HomeComponent  },
+  { path: 'quiensoy' , component : QuiensoyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './nav/header/header.component';
 import { FooterComponent } from './nav/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuiensoyComponent } from './pages/quiensoy/quiensoy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    QuiensoyComponent
   ],
   imports: [
     BrowserModule,
